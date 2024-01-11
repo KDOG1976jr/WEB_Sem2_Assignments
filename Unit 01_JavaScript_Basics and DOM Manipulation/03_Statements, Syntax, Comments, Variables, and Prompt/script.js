f79a3b719245c5ca056f69a94edf236412eb7e4d
Keith Owens Jr <owenskei000@warren.k12.in.us>
2024-01-11 09:03:12 -0500

    k

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,5 @@
+let First = window.prompt("What is your first name");
+let Last = window.prompt("What is your last name");
+Name = First + " " + Last
+
+document.getElementById("name").innerHTML = Name;
