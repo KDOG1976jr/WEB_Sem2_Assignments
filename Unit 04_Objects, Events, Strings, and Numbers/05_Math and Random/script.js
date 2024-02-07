c3b00e82c7c85ac2493376002946b804f5ad5ff2
Keith Owens Jr. <owenskei000@warren.k12.in.us>
2024-02-07 18:28:49 -0500

    mathing

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -12,4 +12,25 @@ function takeAbsoluteValue() {
     let absoluteValue = Math.abs(number);
     document.getElementById("result").innerHTML = absoluteValue;
 }
+
+function takeSine() {
+    let number = +document.getElementById("input3").value;
+
+    let absoluteValue = Math.sin(number);
+    document.getElementById("result").innerHTML = Sine;
+}
+
+function takeCosine() {
+    let number = +document.getElementById("input4").value;
+
+    let absoluteValue = Math.cos(number);
+    document.getElementById("result").innerHTML = Cosine;
+}
+
+function takeTangent() {
+    let number = +document.getElementById("input5").value;
+
+    let absoluteValue = Math.tan(number);
+    document.getElementById("result").innerHTML = Tangent;
+}
 // Make another function for takeAbsoluteValue
