9574e87d02588cb23dfc3bf672c7cb06000ba854
Keith Owens Jr <owenskei000@warren.k12.in.us>
2024-03-11 09:13:18 -0400

    j

--- Unit 08_Data Structures/01_Arrays/script.js
+++ Unit 08_Data Structures/01_Arrays/script.js
@@ -1,4 +1,4 @@
-const numbers = []
+const numbers = [7, 5, 9, 75, 97, 79];
 
 for(let i = 0; i < 10; i++){
     let num = Math.floor(Math.random() * 10) + 1;
@@ -7,5 +7,7 @@ for(let i = 0; i < 10; i++){
 
 document.getElementById("unsorted").innerHTML = numbers;
 
+numbers.sort(function(a, b){return a - b});
+document.getElementById.innerHTML =
 //YOUDO:  finish the sort and print to the sorted id
 
