15894956c61e1e2c5fad5d9ba02ff078f2d5409d
Keith Owens Jr <owenskei000@warren.k12.in.us>
2024-03-15 08:40:54 -0400

    ki

--- Unit 08_Data Structures/02_Arrays again/script.js
+++ Unit 08_Data Structures/02_Arrays again/script.js
@@ -1,14 +1,14 @@
-// const numbers = []
+ const numbers = []
 
-// for (let i = 0; i < 10; i++) {
-//     let num = Math.floor(Math.random() * 10) + 1;
-//     if(num % 2 !== 0){
-//         numbers.push(num);
-//     }
+ for (let i = 0; i < 10; i++) {
+     let num = Math.floor(Math.random() * 10) + 1;
+     if(num % 2 !== 0){
+         numbers.push(num);
+     }
 
-// }
+ }
 
-// document.getElementById("unsorted").innerHTML = numbers;
+ document.getElementById("unsorted").innerHTML = numbers;
 
 // //YOUDO:  finish the sort and print to the sorted id
 
@@ -28,3 +28,4 @@ let filteredNumbers = numbers.filter(filterOutEvens);
 
 
 document.getElementById("unsorted").innerHTML = filteredNumbers;
+
